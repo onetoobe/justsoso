5abb7017342016c4d4575f70ba5bf7a1a97304a3
lizheng <lz3177768903@gmail.com>
2019-07-24 21:04:11 +0800

    kljdla

--- untitled/wait/chara.py
+++ untitled/wait/chara.py
@@ -25,12 +25,28 @@ class Fab:
 
     def __iter__(self):
         return self
+# class Fab:
+#     def __init__(self, max):
+#         self.max = max
+#         self.n, self.a, self.b = 0, 0, 1
+#
+#     def __next__(self):
+#         if self.n < self.max:
+#             r = self.b
+#             self.a, self.b = self.b, self.a + self.b
+#             self.n = self.n + 1
+#             return r
+#         raise StopIteration
+#
+#
+#     def __iter__(self):
+#         return self
 
 
 # print(Fab(5))
 
-for n in Fab(5):
-    print(n)
+# for n in Fab(5):
+#     print(n)
 #
 #
 # li=[1,2,3,4]
